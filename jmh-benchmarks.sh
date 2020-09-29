8b4f896089d23016ddb7e67460897a0d8f28cac5
Tomasz Posluszny <tefposluszny@gmail.com>
2020-09-30 00:50:22 +0200

    Fix JMH run script

--- jmh-benchmarks.sh
+++ jmh-benchmarks.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-java -Djava.library.path=target/jni-construction-benchmark-1.0-SNAPSHOT-application/jni-construction-benchmark-1.0-SNAPSHOT/lib -jar target/jni-construction-benchmark-1.0-SNAPSHOT-benchmarks.nar $@ -rf csv
+java -Djava.library.path=target/jni-benchmarks-1.0.0-SNAPSHOT-application/jni-benchmarks-1.0.0-SNAPSHOT/lib -jar target/jni-benchmarks-1.0.0-SNAPSHOT-benchmarks.nar $@ -rf csv
